4274fbc55cd38cc55ee21c14358c85ca37661c0a
Kianna Quach <kiannaquach.kq@gmail.com>
2019-03-25 21:24:16 -0700

    set up authentication for login and registration

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
-// Connect to MongoDB
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// MongoDB connection
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB succesfully connected!'))
-    .catch(err => console.log('err'));
-    
-app.get('/', (req, res) => res.send('Hello'));
+  .connect(db)
+  .then(() => console.log('MongoDB succesfully connected!'))
+  .catch(err => console.log('err'));
+
+// Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 8000;
 
---
+++ validation/register.js
@@ -0,0 +1,48 @@
+const Validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = function vaildateRegisterInput(data) {
+  let errors = {};
+
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.confirmPassword = !isEmpty(data.confirmPassword) ? data.confirmPassword : '';
+
+  if (!Validator.isLength(data.name, {min: 2, max: 50})) {
+    errors.name = 'Name must be between 2 and 50 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name is required';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required';
+  }
+
+  if (!Validator.isLength(data.password, {min: 1, max: 15})) {
+    errors.password = 'Password must be between 1 and 15 characters';
+  }
+
+  if (Validator.isEmpty(data.confirmPassword)) {
+    errors.confirmPassword = 'Password confirmation failed';
+  }
+
+  if (Validator.equals(data.password, data.confirmPassword)) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+
+  return {
+    errors, 
+    isValid: isEmpty(errors),
+  }
+}
