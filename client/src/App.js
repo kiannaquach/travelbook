4a365aca12ad8a269834fe3e291e4bad3a674269
Kianna Quach <kiannaquach.kq@gmail.com>
2019-04-05 20:41:50 -0700

    Set up navbar structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <Navbar />
       </div>
     );
   }
