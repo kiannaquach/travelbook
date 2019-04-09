b3322607cf89d8e284568fea34c1415f1f070829
Kianna Quach <kiannaquach.kq@gmail.com>
2019-04-08 19:45:52 -0700

    set up login and register forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const loginUser = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    console.log(loginUser);
+  }
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your Travelbook account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    placeholder="Email Address"
+                    name="email"
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    className='form-control form-control lg'
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    className='form-control form-control lg'
+                  />
+                </div>
+                <div className='form-group'>
+                  <input type="submit" className="btn btn-info btn-block mt-4" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
