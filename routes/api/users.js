8b4be3898001cb9c042de6b4e78b9ff3262b3de3
Kianna Quach <kiannaquach.kq@gmail.com>
2019-04-20 21:21:07 -0700

    fix authenication issues

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,33 +56,33 @@ router.post('/register', (req, res) => {
 // @route   GET api/users/login
 // @desc    User login + return JWT token
 // @access  Public
-router.get('/login', (req, res) => {
+router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginUserInput(req.body);
 
-  if (!isValid) {
+   if (!isValid) {
     return res.status(400).json(errors);
   }
 
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ email })
+   User.findOne({ email })
     .then(user => {
       if (!user) {
         errors.email = 'User not found';
 
-        return res.status(404).json(errors);
+         return res.status(404).json(errors);
       }
 
-      bcrypt.compare(password, user.password)
+       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
             const payload = { id: user.id, name: user.name, avatar: user.avatar }
 
-            jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+             jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
               res.json({
                 success: true,
-                token: `Bearer ${token}`,
+                token: 'Bearer ' + token,
               });
             });
           } else {
