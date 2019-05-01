4a545aed64ad2f98b0181fa64b5c87e0043e4d3b
Kianna Quach <kiannaquach.kq@gmail.com>
2019-04-30 20:45:02 -0700

    Fix some errors in validation

--- validation/login.js
+++ validation/login.js
@@ -1,27 +1,26 @@
 const Validator = require('validator');
 const isEmpty = require('./isEmpty');
 
-module.exports = function vaildateLoginInput(data) {
+module.exports = function validateLoginInput(data) {
   let errors = {};
 
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
 
-  
   if (!Validator.isEmail(data.email)) {
     errors.email = 'Email is invalid';
   }
-  
+
   if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email is required';
+    errors.email = 'Email field is required';
   }
-  
+
   if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password is required';
+    errors.password = 'Password field is required';
   }
 
   return {
-    errors, 
-    isValid: isEmpty(errors),
-  }
-}
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
