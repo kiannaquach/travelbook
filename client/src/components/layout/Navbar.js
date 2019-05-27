0e4cf09aff00d411df68634e7c8627be0d084919
Kianna Quach <kiannaquach.kq@gmail.com>
2019-05-26 22:58:23 -0700

    Add emojis to nabber links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -21,17 +21,17 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to='/profiles'>Travelers</Link>
+                <Link className="nav-link" to='/profiles'>Travelers✈️</Link>
               </li>
             </ul>
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to='/register'>Sign Up</Link>
+                <Link className="nav-link" to='/register'>Sign Up🏄🏻‍♀️</Link>
               </li>
 
               <li className="nav-item">
-                <Link className="nav-link" to='/login'>Login</Link>
+                <Link className="nav-link" to='/login'>Login🌍</Link>
               </li>
             </ul>
           </div>
