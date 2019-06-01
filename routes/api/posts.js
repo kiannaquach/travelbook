693d20928220713bfb99999d1d9b7f3674729a22
Kianna Quach <kiannaquach.kq@gmail.com>
2019-05-31 21:16:54 -0700

    Fix validation error

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -68,7 +68,7 @@ router.delete(
         Post.findById(req.params.id)
           .then(post => {
             if (post.user.toString() !== req.user.id) {
-              return res.status(401).json({ notauthorized: 'User not authorized'});
+              return res.status(401).json({ notauthorized: 'User is not authorized'});
             }
 
             post.remove().then(() => res.json({ success: true }))
